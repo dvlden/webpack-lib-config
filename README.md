6deeebe808015108d8b1a234550fba16bac83742
Nenad Novaković <n.dvLden@gmail.com>
2018-08-11 18:18:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Webpack Config
+
+Config is fully transparent. You don't need to touch it, unless you want
+to implement some new plugins or change module format.
+
+It is currently set to UMD pattern, but if you'd like to change format,
+that is something you can definitely do. For the purpose of module building
+it is as minimal as possible. Also, it resolves the module name automatically.
+
+
+## Getting started
+
+- `git clone https://github.com/dvlden/webpack-module-config.git`
+- `npm i`
+
+
+### Commands
+
+- `npm run dev` - build for development
+- `npm run build` - build for production
+
+
+## Important Notes
+
+If you cloned my repository and want to use this for modules development, before
+you push any changes to Git or Npm, make sure to update some important files.
+
+- package.json
+- readme
+- license
+
+&nbsp;
+
+> Developed by Nenad Novaković, dvlden.
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const pkg = require('./package.json')
+
+const plugins = {
+  clean: require('clean-webpack-plugin')
+}
+
+const libName = (name) => (
+  name.split('-').map(word =>
+    word.charAt(0).toUpperCase() + word.slice(1)
+  ).join('')
+)
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+
+  entry: {
+    index: './index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/',
+    filename: '[name].js',
+    library: libName(pkg.name),
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    globalObject: 'typeof self !== \'undefined\' ? self : this', // temporary, waiting for Webpack fix
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+
+  plugins: [
+    new plugins.clean(['dist'])
+  ],
+
+  devtool: ''
+}
